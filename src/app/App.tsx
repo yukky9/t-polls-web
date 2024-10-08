83152f964a09ffcaa8f175f425e2de0fac64914d
yukky9 <kozhurinayulia@gmail.com>
2024-10-09 00:09:42 +0300

    start

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../app/index.css';
+import MainRouter from "../router/MainRouter";
+
+function App() {
+  return (
+      <div>
+          <MainRouter/>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/router/MainRouter.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function MainRouter() {
+    return (
+        <BrowserRouter>
+            <Routes>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default MainRouter;
