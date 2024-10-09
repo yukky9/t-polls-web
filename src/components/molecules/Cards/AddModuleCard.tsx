44dec914c0459030bce39daa0ecd06243686c8fb
yukky9 <kozhurinayulia@gmail.com>
2024-10-09 22:14:28 +0300

    correct

--- src/components/molecules/Cards/AddModuleCard.tsx
+++ src/components/molecules/Cards/AddModuleCard.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import AddPollsButton from "../../atoms/IconButton/AddPollsButton/AddPollsButton";
+import addPattersIcon from '../../../assets/Plus.png'
 
 
 const AddModuleCard = () => {
     return (
-        <button className='bg-gray-300 w-72 pl-5 rounded-2xl items-center justify-center'>
-            <AddPollsButton/>
+        <button className='bg-gray-300 w-72 pl-5 rounded-2xl'>
+            <div className='pl-28'>
+                <img className='w-5 h-5' alt='AddPattersIcon' src={addPattersIcon}/>
+            </div>
         </button>
     );
 };
